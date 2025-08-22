ceadd1666dc9041d58eaedbba19526e95ac04fc5
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-08-22 18:41:28 +0300

    Refactored snake as class, controls work

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,6 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,29 +8,19 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.5)
-
-    for i in range(len(segments)-1, 0, -1):
-        new_x = segments[i - 1].xcor()
-        new_y = segments[i - 1].ycor()
-        segments[i].goto(new_x, new_y)
-    segments[0].forward(20)
-
+    snake.move()
 
 screen.exitonclick()
 
