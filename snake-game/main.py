78349558933727240006513283d32348290ba90b
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-08-23 12:39:33 +0300

    Snake can collide with itself

--- snake-game/main.py
+++ snake-game/main.py
@@ -38,5 +38,11 @@ while is_game_on:
         snake.head.color("red")
         is_game_on = False
 
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment != snake.head and snake.head.distance(segment) < 15:
+            scoreboard.game_over()
+            is_game_on = False
+
 screen.exitonclick()
 
