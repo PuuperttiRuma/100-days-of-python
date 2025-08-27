ae54fb52ffff291eed7610d95c7728b7b487a59e
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-08-27 10:12:39 +0300

    Day 24: Highscore for Snake Game

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -11,7 +11,21 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 260)
         self.score = 0
+        self.highscore = self.get_highscore()
         self.update_scoreboard()
+
+    def get_highscore(self):
+        with open("highscore.txt") as file:
+            try:
+                highscore = int(file.read())
+            except ValueError:
+                print("Error reading highscore file!")
+                return 0
+        return highscore
+
+    def write_highscore(self):
+        with open("highscore.txt", "w") as file:
+            file.write(f"{self.highscore}")
 
     def update_scoreboard(self):
         self.clear()
@@ -22,7 +36,14 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def game_over(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.write_highscore()
         self.goto(0,0)
         self.write("GAME OVER", False, ALIGNMENT, FONT)
+        self.goto(0,-30)
+        self.write(f"High score: {self.highscore}", False, ALIGNMENT, FONT)
+        # self.goto(0, -120)
+        # self.write("(Press 'Enter' to play again, 'q' to quit)", False, ALIGNMENT, FONT)
 
 
